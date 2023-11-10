57ab3471390519c90f1ce8d1364ce19d47bdc074
bilawal.shaikh@gmail.com <bilawal.shaikh027@gmail.com>
2023-11-10 12:07:16 +0500

    adding new updated code

--- app.js
+++ app.js
@@ -1,46 +1,111 @@
+// initialize empty array
+const arr = [];
+
 const inputElement = document.getElementById("myInput");
 const addBtn = document.getElementById("addTask");
 const taskList = document.getElementById("taskList");
 
-addBtn.addEventListener("click", function () {
-  if (inputElement.value !== "") {
-    //created 3 elemets
-    const li = document.createElement("li");
-    const buttonRemove = document.createElement("button");
-    const buttonCompleted = document.createElement("button");
+//check
 
-    //added remove btn text and class
-    buttonRemove.textContent = "Remove";
-    buttonRemove.className = "Cross";
+const checkbtn = document.getElementById("checkbtn");
 
-    //added completed btn text and class
-    buttonCompleted.textContent = "Completed";
-    buttonCompleted.className = "Completed";
+checkbtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(arr);
+});
 
-    //set the inner text of li
-    li.innerText = inputElement.value;
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  handleFormAction();
+});
 
-    //adding both the buttons to li
-    li.appendChild(buttonCompleted);
-    li.appendChild(buttonRemove);
-
-    //added li in ul
-    taskList.appendChild(li);
-
-    //setting input value = null
-    inputElement.value = "";
-
-    //event handler for completed button
-    buttonCompleted.addEventListener("click", function (e) {
-      // e.target.style.textDecoration = "line-through";
-      li.style.textDecoration = "line-through";
-    });
-
-    //event handler for remove button
-    buttonRemove.addEventListener("click", function (e) {
-      li.remove();
-    });
-  } else {
-    alert("please enter text");
+inputElement.addEventListener("keypress", (e) => {
+  // Check if the pressed key is Enter
+  if (e.key === "Enter") {
+    e.preventDefault();
+    handleFormAction();
   }
 });
+
+function handleFormAction() {
+  //check if user click with mouse or pad
+  if (inputElement.value !== "") {
+    add();
+    render();
+  } else {
+    alert("enter value");
+  }
+}
+
+function add() {
+  arr.push({
+    text: inputElement.value,
+    isChecked: false,
+    id: Date.now(),
+  });
+
+  render();
+}
+
+function render() {
+  // Clear the existing content of ul
+  taskList.innerHTML = "";
+
+  arr.forEach((e) => {
+    const li = document.createElement("li");
+    li.textContent = e.text;
+
+    // Icon
+    const crossIcon = document.createElement("i");
+    crossIcon.innerHTML = "✖";
+    crossIcon.className = "crossIcon";
+
+    crossIcon.addEventListener("click", handleDelete);
+    crossIcon.setAttribute("x-id", e.id);
+
+    // Checkbox
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "checkbox";
+    checkbox.addEventListener("change", handleCheckboxChange);
+    checkbox.setAttribute("c-id", e.id);
+
+    if (e.isChecked) {
+      checkbox.checked = true;
+    }
+
+    li.appendChild(checkbox);
+    li.appendChild(crossIcon);
+
+    // Append the new list item to the taskList
+    taskList.appendChild(li);
+  });
+
+  inputElement.value = "";
+}
+
+function handleDelete(event) {
+  const clickedId = event.target.getAttribute("x-id");
+  arr.forEach((item, index, arr) => {
+    if (item.id == clickedId) {
+      arr.splice(index, 1);
+      // event.target.parentNode.remove();
+    }
+  });
+  render();
+}
+
+function handleCheckboxChange(event) {
+  const checkbox = event.target;
+  const clickedId = event.target.getAttribute("c-id");
+
+  arr.forEach((item, index, arr) => {
+    if (item.id == clickedId && checkbox.checked) {
+      event.target.parentNode.style.textDecoration = "line-through";
+      arr[index].isChecked = true;
+    } else if (!checkbox.checked) {
+      arr[index].isChecked = false;
+      event.target.parentNode.style.textDecoration = "none";
+    }
+  });
+}
